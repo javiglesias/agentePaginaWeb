05263d0d260bab087d2c0e724ef7078620cbafb4
javiglesias <javiglesias18@gmail.com>
2014-10-27 13:43:08 +0100

    NO me crea el el agente server.
    He copiado la clase server del ejercicio 1 y no me crea el agente en la
    plataforma JADE.

--- src/Agents/agenteWeb.java
+++ src/Agents/agenteWeb.java
@@ -10,27 +10,24 @@ public class agenteWeb extends Agent
 		private static final long serialVerisonUID = 1L;
 		protected CyclicBehaviour cyclicBehaviour;
 		protected ParallelBehaviour parallelBehaviour;
-		Scanner sc = new Scanner(System.in);
-		String message;
 		
 			public void setup()
 			{
 				cyclicBehaviour = new CyclicBehaviour(this)
 				{
-
 					@Override
 					public void action() {
 						// TODO Auto-generated method stub
-						
+						block();
 					}
 					
 				};
-				parallelBehaviour = new ParallelBehaviour(this,0)
+				/*parallelBehaviour = new ParallelBehaviour(this,0)
 				{
 					
-				};
+				};*/
 				addBehaviour(cyclicBehaviour);
-				addBehaviour(parallelBehaviour);
+				//addBehaviour(parallelBehaviour);
 			}
 	}
 
